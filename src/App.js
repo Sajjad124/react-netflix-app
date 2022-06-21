4b28cb77dddeaa483104618c796c85b8f4cc14db
Sajjad124 <181370124@gift.edu.pk>
2022-06-21 23:44:26 +0500

    addition of navbar and changes in styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Card } from "./Card";
-import extracurricular from './images/extracurricular.jpg';
+import { TopBar } from "./TopBar";
+import extracurricular from "./images/extracurricular.jpg";
 
 function App() {
   return (
-    <div className="cards_container">
-      <Card
-        src="https://wallpapercave.com/wp/wp4056410.jpg"
-        title="A Netflix original series"
-        sname="DARK"
-        link="https://www.youtube.com/watch?v=rrwycJ08PSA"
-      />
-      <Card
-        src={extracurricular}
-        title="A Netflix original series"
-        sname="Extra Curricular"
-        link="https://www.youtube.com/watch?v=aV_DBz2rKsI"
-      />
-      <Card
-        src="https://wallpapercave.com/wp/wp1917154.jpg"
-        title="A Netflix original series"
-        sname="Stranger Things"
-        link="https://www.youtube.com/watch?v=b9EkMc79ZSU"
-      />
-    </div>
+   <>
+      
+        <TopBar />
+      
+      <div className="cards_container">
+     
+        <Card
+          src="https://wallpapercave.com/wp/wp4056410.jpg"
+          title="A Netflix original series"
+          sname="DARK"
+          link="https://www.youtube.com/watch?v=rrwycJ08PSA"
+        />
+        <Card
+          src={extracurricular}
+          title="A Netflix original series"
+          sname="Extra Curricular"
+          link="https://www.youtube.com/watch?v=aV_DBz2rKsI"
+        />
+        <Card
+          src="https://wallpapercave.com/wp/wp1917154.jpg"
+          title="A Netflix original series"
+          sname="Stranger Things"
+          link="https://www.youtube.com/watch?v=b9EkMc79ZSU"
+        />
+      </div>
+      </>
   );
 }
 
