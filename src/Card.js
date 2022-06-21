938baa06706701d34d0f931c4a84ca2eb91c2d8d
Sajjad124 <181370124@gift.edu.pk>
2022-06-21 18:29:08 +0500

    Initial commit

---
+++ src/Card.js
@@ -0,0 +1,28 @@
+import React from "react";
+import App from "./App";
+
+
+function Card(props) {
+    return (
+        <div className="card">
+          <img
+            src={props.src}
+            alt="mypic"
+            className="card__img"
+          />
+          <div className="card__info">
+            <span className="card_category">{props.title} </span>
+            <h3 className="card__title">{props.sname}</h3>
+            <a
+              href={props.link}
+              target="_blank"
+            >
+              <button>Watch now</button>
+            </a>
+          </div>
+        </div>
+    );
+  }
+
+  export {Card};
+  
