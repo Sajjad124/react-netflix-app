94d8f5f2a397aacec6d7117edb186fe6f80a2f99
Muhammad Umair <181370124@gift.edu.pk>
2023-01-03 13:44:25 +0500

    update

--- src/App.js
+++ src/App.js
@@ -6,28 +6,29 @@ import Netflix from "./Netflix";
 import Amazon from "./Amazon";
 
 const favSeries = "Amazon";
-// const FavS = () => {
-//   if (favSeries === "Netflix") {
-//     return <Netflix />;
-//   } else {
-//     return <Amazon />;
-//   }
-// };
+const FavS = () => {
+  if (favSeries === "Netflix") {
+    return <Netflix />;
+  } else {
+    return <Amazon />;
+  }
+};
 const App = () => {
-   return (
+  return (
     <>
-      {/* <div>
+      <div>
         <TopBar />
       </div>
       <div className="cards_container">
         {/* fat arrow function */}
-        {/* {Sdata.map((value, index) => { */} */}
-          return
-          {favSeries === "Netflix" ? <Netflix /> : <Amazon /> }
-        {/* })} */}
-      {/* </div> */}
+        {Sdata.map((value, index) => {
+          return(
+           <FavS/>
+          )
+        })}
+      </div>
     </>
-   );
+  );
 };
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,10 @@
 import React from "react";
-import App from "./App";
 import Images from "./Images";
 
 function Card(props) {
   return (
     <div className="card">
-      <Images src={props.src}/>
+      <Images src={props.src} />
       <div className="card__info">
         <span className="card_category">{props.title} </span>
         <h3 className="card__title">{props.sname}</h3>
